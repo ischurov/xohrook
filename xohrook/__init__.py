017618997bae576b6fcba72947ccb37a9d52328b
Yury G. Kudryashov <urkud.urkud@gmail.com>
2013-10-16 16:14:30 +0400

    Remove SQLA session on teardown_request

--- xohrook/__init__.py
+++ xohrook/__init__.py
@@ -4,4 +4,8 @@ app = Flask( __name__, instance_relative_config = True )
 
 app.config.from_pyfile( 'xohrook.cfg' )
 
+@app.teardown_request
+def shutdown_session(exception=None):
+    from .db.flask import db_session
+    db_session.remove()
 from .views import *
