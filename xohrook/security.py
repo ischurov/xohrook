b47663389168e6eb9b824e9f1787aef526192f6e
Yury G. Kudryashov <urkud.urkud@gmail.com>
2013-11-20 12:19:28 +0400

    Introduce IdMixin
    This will make possible using multi-column ids. This can be useful for "join" tables with extra information.
    
    Also, add __all__ to some source files.

--- xohrook/security.py
+++ xohrook/security.py
@@ -3,6 +3,8 @@ from flask.ext.security import Security, SQLAlchemyUserDatastore
 
 from .models.users import User, Role
 
+__all__ = ['init_security']
+
 def init_security(app, db):
     user_datastore = SQLAlchemyUserDatastore(db, User, Role)
     return Security(app, user_datastore)
